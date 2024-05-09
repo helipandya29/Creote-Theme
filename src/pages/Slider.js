06251c43842d0979cd8ba554e7e320b354736769
DarshanZala030 <zdarshan677@gmail.com>
2024-05-09 18:34:25 +0530

    .

--- src/pages/Slider.js
+++ src/pages/Slider.js
@@ -51,21 +51,20 @@ import axios from 'axios';
 
 const Slider = () => {
   const [users, setUsers] = useState([]);
-
+  const imageUrl = "assets/images/sliders/slider-2-bg.jpg"
   useEffect(() => {
-    axios.get('http://localhost:5000/getUsers')
+    axios.get('http://localhost:5000/api/creote/getUsers')
       .then(response => setUsers(response.data))
       .catch(error => console.log(error));
   }, []);
 
   return (
     <section className="slider style_one pag_position_two">
-      <div className="banner_carousel owl-carousel owl_nav_none owl_dots_block theme_carousel owl-theme" 
-        data-options='{"loop": true, "margin": 0, "autoHeight":true, "lazyLoad":true, "nav": true, "dots": true, "autoplay": true, "autoplayTimeout": 7000, "smartSpeed": 1800, "responsive": { "0": { "items": "1" }, "768": { "items": "1" }, "1000": { "items": "1" }}}'>
+     <div className="banner_carousel owl-carousel owl_nav_none owl_dots_block theme_carousel owl-theme" data-options="{&quot;loop&quot;: true, &quot;margin&quot;: 0, &quot;autoheight&quot;:true, &quot;lazyload&quot;:true, &quot;nav&quot;: true, &quot;dots&quot;: true, &quot;autoplay&quot;: true, &quot;autoplayTimeout&quot;: 7000, &quot;smartSpeed&quot;: 1800, &quot;responsive&quot;:{ &quot;0&quot; :{ &quot;items&quot;: &quot;1&quot; }, &quot;768&quot; :{ &quot;items&quot; : &quot;1&quot; } , &quot;1000&quot;:{ &quot;items&quot; : &quot;1&quot; }}}">
         {users.map(user => (
           <div className="slide-item" key={user._id}>
             <div className="slide-item-content content_left">
-              <div className="image-layer" style={{backgroundImage: `url(${user.imageUrl})`}}></div>
+              <div className="image-layer" style={{backgroundImage: `url(${imageUrl})`}}></div>
               <div className="auto-container">
                 <div className="d-flex align-items-center">
                   <div className="col-lg-7 col-md-12  col-sm-12 col-xs-12">
@@ -78,7 +77,7 @@ const Slider = () => {
                   </div>
                   <div className="col-lg-5 col-md-12  col-sm-12 col-xs-12 image_column">
                     <div className="slider_image">
-                      <img src={user.imageSrc} className="img-fluid" alt="slider"/>
+                      <img src="assets/images/sliders/slider-2-3.png" className="img-fluid" alt="slider"/>
                     </div>
                   </div>
                 </div>
--- src/pages/Tab.js
+++ src/pages/Tab.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 const imageUrl5 = "assets/images/tab-sec-bg.jpg";
 const imageUrl6 = "assets/images/tab-image.jpg";
 const imageUrl7 = "assets/images/blog-image-8.jpg";
@@ -7,6 +7,12 @@ const imageUrl9 = "assets/images/about-2.jpg";
 const imageUrl10 = "assets/images/about-3.jpg";
 
 const TabSection = () => {
+    const [activeTab, setActiveTab] = useState("#tabtabone");
+
+    const handleTabClick = (tabId) => {
+        setActiveTab(tabId);
+     };
+
     return (
         <>
             {/* <!---tab----> */}
@@ -21,19 +27,24 @@ const TabSection = () => {
                                 <div className="tabs_header clearfix">
                                     <ul className="showcase_tabs_btns nav-pills nav   clearfix">
                                         <li className="nav-item">
-                                            <a href="/" className="s_tab_btn nav-link active" data-tab="#tabtabone">01. Affordable</a>
+                                        <a className={`s_tab_btn nav-link ${activeTab === "#tabtabone" ? "active" : ""}`}   onClick={() => handleTabClick("#tabtabone")}>01. Affordable</a>
+                                            {/* <a href="/" className="s_tab_btn nav-link active" data-tab="#tabtabone">01. Affordable</a> */}
                                         </li>
                                         <li className="nav-item">
-                                            <a href="/" className="s_tab_btn nav-link" data-tab="#tabtabtwo">02. Knowledge</a>
+                                        <a className={`s_tab_btn nav-link ${activeTab === "#tabtabtwo" ? "active" : ""}`} onClick={() => handleTabClick("#tabtabtwo")}>02. Knowledge</a>
+                                            {/* <a href="/" className="s_tab_btn nav-link" data-tab="#tabtabtwo">02. Knowledge</a> */}
                                         </li>
                                         <li className="nav-item">
-                                            <a href="/" className="s_tab_btn nav-link" data-tab="#tabtabthree">03. Saves Time</a>
+                                        <a className={`s_tab_btn nav-link ${activeTab === "#tabtabthree" ? "active" : ""}`} onClick={() => handleTabClick("#tabtabthree")}>03. Saves Time</a>
+                                            {/* <a href="/" className="s_tab_btn nav-link" data-tab="#tabtabthree">03. Saves Time</a> */}
                                         </li>
                                         <li className="nav-item">
-                                            <a href="/" className="s_tab_btn nav-link" data-tab="#tabtabtfour">04. Fast &amp; Quality</a>
+                                        <a className={`s_tab_btn nav-link ${activeTab === "#tabtabfour" ? "active" : ""}`} onClick={() => handleTabClick("#tabtabfour")}>04. Fast &amp; Quality </a>
+                                            {/* <a href="/" className="s_tab_btn nav-link" data-tab="#tabtabtfour">04. Fast &amp; Quality</a> */}
                                         </li>
                                         <li className="nav-item">
-                                            <a href="/" className="s_tab_btn nav-link" data-tab="#tabtabfive">05. Experienced</a>
+                                        <a className={`s_tab_btn nav-link ${activeTab === "#tabtabfive" ? "active" : ""}`} onClick={() => handleTabClick("#tabtabfive")}>05. Experienced</a>
+                                            {/* <a href="/" className="s_tab_btn nav-link" data-tab="#tabtabfive">05. Experienced</a> */}
                                         </li>
                                     </ul>
                                     <div className="toll_free">
@@ -42,7 +53,7 @@ const TabSection = () => {
                                 </div>
                                 <div className="s_tab_wrapper">
                                     <div className="s_tabs_content">
-                                        <div className="s_tab fade active-tab show" id="tabtabone">
+                                    <div className={`s_tab fade ${activeTab === "#tabtabone" ? "active-tab show" : ""}`} id="tabtabone">
                                             <div className="tab_content one" style={{ backgroundImage: `url(${imageUrl6})` }}>
                                                 <div className="content_image">
                                                     <h6>Why Creote</h6>
@@ -52,7 +63,7 @@ const TabSection = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="s_tab fade" id="tabtabtwo">
+                                        <div className={`s_tab fade ${activeTab === "#tabtabtwo" ? "active-tab show" : ""}`} id="tabtabtwo">
                                             <div className="tab_content one" style={{ backgroundImage: `url(${imageUrl7})` }}>
                                                 <div className="content_image">
                                                     <h6>Why Creote</h6>
@@ -62,7 +73,7 @@ const TabSection = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="s_tab fade" id="tabtabthree">
+                                        <div className={`s_tab fade ${activeTab === "#tabtabthree" ? "active-tab show" : ""}`} id="tabtabthree">
                                             <div className="tab_content one" style={{ backgroundImage: `url(${imageUrl8})` }}>
                                                 <div className="content_image">
                                                     <h6>Why Creote</h6>
@@ -72,7 +83,7 @@ const TabSection = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="s_tab fade" id="tabtabtfour">
+                                        <div className={`s_tab fade ${activeTab === "#tabtabfour" ? "active-tab show" : ""}`} id="tabtabfour">
                                             <div className="tab_content one" style={{ backgroundImage: `url(${imageUrl9})` }}>
                                                 <div className="content_image">
                                                     <h6>Why Creote</h6>
@@ -82,7 +93,7 @@ const TabSection = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="s_tab fade" id="tabtabfive">
+                                        <div className={`s_tab fade ${activeTab === "#tabtabfive" ? "active-tab show" : ""}`} id="tabtabfive">
                                             <div className="tab_content one" style={{ backgroundImage: `url(${imageUrl10})` }}>
                                                 <div className="content_image">
                                                     <h6>Why Creote</h6>
@@ -108,3 +119,4 @@ const TabSection = () => {
 };
 
 export default TabSection;
+
